c480bcf54a3dec59e5bce14ce70b2bc283ac4382
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-16 18:32:56 +0000

    added capacity property to library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class Library {
 
+    private int capacity;
     private ArrayList<Book> bookCollection;
 
-    public Library(){
+    public Library(int capacity){
+        this.capacity = capacity;
         this.bookCollection = new ArrayList<Book>();
     }
 
