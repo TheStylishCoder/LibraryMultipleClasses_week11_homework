eb554bf4754f95037313c8c577191ea167814436
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-16 18:27:50 +0000

    first library test passes

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("Strangers on a Train", "Patricia Highsmith", "crime");
+
+    }
+
+    @Test
+    public void canCountBooksInStock(){
+        assertEquals(0, library.stockCount());
+    }
+
+
 }
